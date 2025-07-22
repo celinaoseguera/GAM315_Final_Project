87c3176deaae51d61d1871c240aa36e32aa277fe
celinaoseguera <159492239+celinaoseguera@users.noreply.github.com>
2025-07-21 21:04:12 -0700

    Set up callback registration for crops
    Invoke OnCropPlanted and  OnCropWatered to run whatever callbacks we register over in the crop script

--- Assets/_Project/Scripts/GrowableSoil.cs
+++ Assets/_Project/Scripts/GrowableSoil.cs
@@ -9,17 +9,32 @@ public class GrowableSoil : MonoBehaviour
     [SerializeField] InputPublisher inputPublisher;
     private bool playerPresent = false;
     private const string PLAYER_TAG = "Player";
+    private SpriteRenderer spriteRenderer;
+    private bool cropWatered = false;
+
+    public event EventHandler OnCropPlanted;
+    public event EventHandler OnCropWatered;
+    //public event EventHandler OnCropHarvested;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         // only call the water function attached to teh spacebar event IF the player is on top of the soil
-        if (playerPresent == true)
+        // CELINA, do a check for if the plant has already been watered otherwise will put too many calls into callback
+        if (playerPresent == true && cropWatered == false)
         {
+            inputPublisher.OnEPressed += PlantCrops;
             inputPublisher.OnSpacePressed += WaterCrops;
+            cropWatered = true;
+
             // inputPublisher.OnSpacePressed += HarvestCrops;
         }
-        
+
     }
 
     // when player touches the soil trigger
@@ -46,10 +61,19 @@ public class GrowableSoil : MonoBehaviour
         return;
     }
 
+    void PlantCrops(object sender, EventArgs e)
+    {
+        Debug.Log("planted!");
+        OnCropPlanted?.Invoke(this, EventArgs.Empty);
+        // will show the seed sprite
+    }
 
     void WaterCrops(object sender, EventArgs e)
     {
         Debug.Log("plant watered");
+        OnCropWatered?.Invoke(this, EventArgs.Empty);
+        // will set off time delta time clock to change sprite from seed to plant and then to mature
+        spriteRenderer.color = new Color (0f, 0.5019608f, 0.5019608f, .3f);
 
     }
 
--- Assets/_Project/Scripts/InputPublisher.cs
+++ Assets/_Project/Scripts/InputPublisher.cs
@@ -19,6 +19,7 @@ public class InputPublisher : MonoBehaviour
     }
 
     public event EventHandler OnSpacePressed;
+    public event EventHandler OnEPressed;
 
     // Update is called once per frame
     void Update()
@@ -55,9 +56,14 @@ public class InputPublisher : MonoBehaviour
             });
         }
 
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             OnSpacePressed?.Invoke(this, EventArgs.Empty);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            OnEPressed?.Invoke(this, EventArgs.Empty);
         }
 
     }
